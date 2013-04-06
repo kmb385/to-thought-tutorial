212f807cbeaca6d2b3148ca9e235a5a29ba6550e
Kevin Bowersox <kmb385@gmail.com>
2013-04-06 10:52:11 -0400

    save point 2

--- src/test/java/com/cloudfoundry/tothought/PostRepositoryTest.java
+++ src/test/java/com/cloudfoundry/tothought/PostRepositoryTest.java
@@ -1,9 +1,6 @@
 package com.cloudfoundry.tothought;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -17,9 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cloudfoundry.tothought.entities.Comment;
 import com.cloudfoundry.tothought.entities.Post;
-import com.cloudfoundry.tothought.entities.PostPart;
 import com.cloudfoundry.tothought.entities.Stamp;
-import com.cloudfoundry.tothought.entities.Tag;
 import com.cloudfoundry.tothought.repositories.PostRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -33,100 +28,23 @@ public class PostRepositoryTest {
 	PostRepository repository;
 	
 	@Test
-	public void entityCollectionTest(){
-		Post post = repository.findOne(33);
-		int size = post.getUrls().size();
-		
-		assertTrue(size > 0);
-		
-		post.getUrls().add("http://tothought.cloudfoundry.com");
-		
-		repository.save(post);
-		
-		Post dbPost = repository.findOne(33);
-		assertEquals(size+1, dbPost.getUrls().size());
-	}
-	
-	@Test
-	public void test() {
-		String tagName = "Java";
-		String tagName2 = "Spring";
-		
-		Tag tag1 = new Tag();
-		Tag tag2 = new Tag();
-		
-		tag1.setName(tagName);
-		tag2.setName(tagName2);
-		
+	public void orderTest(){
+		List<Comment> comments = new ArrayList<Comment>();
 		Post post = new Post();
-		post.setPostDate(new Date());
-		post.setTitle("First Post");
+		post.setTitle("This is the title");
 		
-		post.getTags().add(tag1);
-		post.getTags().add(tag2);
+		for(int x = 0; x < 30; x++){
+			Comment comment = new Comment();
+			Stamp stamp = new Stamp();
+			
+			stamp.setAuthor("Kevin Bowersox");
+			stamp.setCreatedDate(new Date(113, 01,x));
+			
+			comment.setStamp(stamp);
+			comments.add(comment);
+		}
 		
+		post.setComments(comments);
 		repository.save(post);
-		
-		Post dbpost = repository.findOne(post.getPostId());
-		assertNotNull(dbpost);
-		System.out.println(dbpost.getTitle());
-		
-		List<Tag> tags = dbpost.getTags();
-		
-		assertTrue(tags.size() > 1);
-		assertTrue(tags.contains(tag1));
-		assertTrue(tags.contains(tag2));
 	}
-	
-	@Test
-	public void insertTest(){
-		Comment comment = new Comment();
-		Comment comment2 = new Comment();
-		final String body1 = "This is a test";
-		final String body2 = "This is another test";
-		
-		String author = "Kevin";
-		String email = "kmb385@gmail.com";
-		Date created = new Date();
-		
-		Stamp stamp = new Stamp();
-		stamp.setAuthor(author);
-		stamp.setCreatedDate(created);
-		stamp.setEmail(email);
-		
-		comment.setBody(body1);
-		comment2.setBody(body2);
-		
-		Post post = new Post();
-		post.setPostDate(new Date());
-		post.setTitle("First Post");
-		
-		post.setStamp(stamp);
-		
-		post.getComments().add(comment);
-		post.getComments().add(comment2);
-		
-		PostPart postPart = new PostPart();
-		String body = "Hello";
-		postPart.setBody(body);
-		
-		post.setPostPart(postPart);
-		
-		repository.save(post);
-		
-		Post dbpost = repository.findOne(post.getPostId());
-		assertNotNull(dbpost);
-		assertNotNull(dbpost.getPostPart());
-		assertNotNull(dbpost.getComments());
-		assertEquals(2, dbpost.getComments().size());
-		
-		List<Comment> comments = dbpost.getComments();
-		
-		assertTrue(comments.contains(comment));
-		assertTrue(comments.contains(comment2));
-		System.out.println(dbpost.getTitle());
-		
-		assertEquals(stamp.getAuthor(), dbpost.getStamp().getAuthor());
-	}
-
 }
